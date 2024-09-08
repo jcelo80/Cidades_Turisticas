93bf16b0c49e23874acb419e36ca86bde5a9705c
João Marcelo <joaomarcelosilvalima@gmail.com>
2024-09-08 16:56:45 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        cidade : "Recife",
+        descricao : "O Recife - tradicionalmente precedido pelo artigo - é uma cidade que se destaca em inúmeros aspectos no Brasil. Conhecida por sua influência holandesa, a cidade dos arrecifes, que hoje é um dos centros urbanos mais desenvolvidos do país, preserva muitos resquícios de seu período colonial, garantindo que uma viagem pela cidade revele não apenas as belezas naturais da costa brasileira, como também sua faceta histórica.",
+        link : "https://pt.wikipedia.org/wiki/Recife",
+        tags : "boa viagem cultura praia "
+    },
+    {
+        cidade : "São Paulo",
+        descricao : "São Paulo, a metrópole que nunca dorme, é um caldeirão cultural que reúne pessoas do mundo todo. A cidade que mais cresce na América Latina, São Paulo conjuga a tradição com a modernidade, oferecendo desde museus de renome internacional até uma vida noturna vibrante.",
+        link : "https://pt.wikipedia.org/wiki/S%C3%A3o_Paulo",
+        tags : "garoa predios"
+    },
+    {
+        cidade : "Rio de Janeiro",
+        descricao : "O Rio de Janeiro, a Cidade Maravilhosa, encanta com suas paisagens exuberantes e o clima tropical. A cidade que sediou duas Copas do Mundo e duas Olimpíadas, o Rio de Janeiro é sinônimo de alegria e descontração. Com suas praias, montanhas e florestas, a cidade oferece uma variedade de atividades ao ar livre.",
+        link : "https://pt.wikipedia.org/wiki/Rio_de_Janeiro",
+        tags : "cristo redentor maracana"
+    },
+];
+
